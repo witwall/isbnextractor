79e5b7e50154f3a9e3eb6f883a2a11ef2184592c
Teo.Eterovic <Teo.Eterovic@5119587a-38af-11de-9e80-b52b74bdfc52>
2009-08-14 12:23:46 +0000

    HEAD

---
+++ trunk/ISBNextractor/EnterISBN.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ISBNextractor
+{
+    public partial class EnterISBN : Form
+    {
+        string bookname;
+        string isbn;
+
+        public EnterISBN(string b)
+        {
+            bookname = b;
+            InitializeComponent();
+        }
+
+        private void EnterISBN_Load(object sender, EventArgs e)
+        {
+            bookName.Text = bookname;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            isbn = textBox1.Text;
+            this.Close();
+        }
+        public string Isbn
+        {
+            get
+            {
+                return isbn;
+            }
+            set
+            {
+                isbn = value;
+            }
+        }
+    }
+}
